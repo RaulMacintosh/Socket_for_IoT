f61268327be2a106b7393fd9e7e64d33ec89456d
Raul Silveira <raul95@gmail.com>
2017-04-02 22:15:54 -0300

    Prototype of server between user and core-manager, implemented

--- Client.py
+++ Client.py
@@ -1,17 +1,44 @@
 import socket
 import time
 
-HOST = '192.168.0.117'
-PORT = 5000
-tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-dest = (HOST, PORT)
+from threading import Thread
 
-tcp.connect(dest)
+def client_thread():
+	HOST = '192.168.0.117'
+	PORT = 5000
+	tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	dest = (HOST, PORT)
 
-msg = "Hello\n"
-tcp.send(msg)
+	tcp.connect(dest)
 
-time.sleep(1)
-print tcp.recv(1024)
+	msg = "Hello\n"
+	tcp.send(msg)
 
-tcp.close()
+	time.sleep(1)
+	print tcp.recv(1024)
+
+	tcp_to_thing.close()
+
+def server_thread():
+    HOST = ''
+    PORT = 5001
+    tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    orig = (HOST, PORT)
+    tcp.bind(orig)
+    tcp.listen(1)
+    while True:
+        con, cliente = tcp.accept()
+        print 'Concetado por', cliente
+        while True:
+            msg = con.recv(1024)
+            if not msg: break
+            print cliente, msg
+            con.send("Hello")
+        print 'Finalizando conexao do cliente', cliente
+        con.close()
+
+thread_1 = Thread(target=client_thread)
+thread_2 = Thread(target=server_thread)
+
+#thread_1.start()
+thread_2.start()
