6474e036e3536b43a1fa22668ecc5fd6f5281f40
Raul Silveira <raul95@gmail.com>
2017-04-03 23:59:32 -0300

    Protocol between User and Core-manager defined

--- Client.py
+++ Client.py
@@ -1,23 +1,55 @@
 import socket
 import time
 
-from threading import Thread
+temperature = "25 C\n"
+humidity = "30%\n"
+last_auth = "RFID Tag\n"
+other = "[ERROR] - Command not defined!\n"
 
-def client_thread():
-	HOST = '192.168.0.117'
-	PORT = 5000
-	tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	dest = (HOST, PORT)
+def client_thread(command):
+    # Protocol: 
+    #   * 'a' -> get temperature
+    #   * 'b' -> get humidity
+    #   * 'c' -> get last authentication
+    #   * 'd' -> set on/off red LED
+    #   * 'e' -> set on/off yellow LED
+    #   * 'f' -> set on/off blue LED
 
-	tcp.connect(dest)
+    HOST = '192.168.0.117'
+    PORT = 5000
+    tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    dest = (HOST, PORT)
 
-	msg = "Hello\n"
-	tcp.send(msg)
+    tcp.connect(dest)
 
-	time.sleep(1)
-	print tcp.recv(1024)
+    if command == "TMP":
+        tcp.send('a')
+        time.sleep(1)
+        temperature = tcp.recv(1024)
+    elif command == "HMD":
+        tcp.send('b')
+        time.sleep(1)
+        humidity = tcp.recv(1024)
+    elif command == "AUTH":
+        tcp.send('c')
+        time.sleep(1)
+        last_auth = tcp.recv(1024)
+    elif command == "LED 1":
+        tcp.send('d')
+        time.sleep(1)
+        other = "LED 1"
+    elif command == "LED 2":
+        tcp.send('e')
+        time.sleep(1)
+        other = "LED 2"
+    elif command == "LED 3":
+        tcp.send('f')
+        time.sleep(1)
+        other = "LED 3"
+    else:
+        other = "[ERROR] - Command not defined!"
 
-	tcp_to_thing.close()
+    tcp_to_thing.close()
 
 def server_thread():
     HOST = ''
@@ -28,17 +60,25 @@ def server_thread():
     tcp.listen(1)
     while True:
         con, cliente = tcp.accept()
-        print 'Concetado por', cliente
+        print 'Connected with ', cliente
+
         while True:
-            msg = con.recv(1024)
-            if not msg: break
-            print cliente, msg
-            con.send("Hello")
-        print 'Finalizando conexao do cliente', cliente
+            command = con.recv(2)
+            con.recv(1024)
+
+            if not command: break
+            print cliente, command
+            #client_thread(command)
+            if command == "TP":
+                con.send(temperature)
+            elif command == "HM":
+                con.send(humidity)
+            elif command == "AU":
+                con.send(last_auth)
+            else:
+                con.send(other)
+
+        print 'Finishing connection with ', cliente
         con.close()
 
-thread_1 = Thread(target=client_thread)
-thread_2 = Thread(target=server_thread)
-
-#thread_1.start()
-thread_2.start()
+server_thread()
--- Server/Server.ino
+++ Server/Server.ino
@@ -1,21 +1,31 @@
 #include <SPI.h>
 #include <Ethernet.h>
+#define RED 1
+#define YELLOW 2
+#define BLUE 3
 
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };    // MAC address
-byte ip[] = { 192, 168, 0, 117 };            // IP address. Change when use DHCP
-byte gateway[] = { 192, 168, 0, 1 };  // Router's gateway address. Change when use another network
+byte ip[] = { 192, 168, 25, 8 };            // IP address. Change when use DHCP
+byte gateway[] = { 192, 168, 25, 1 };  // Router's gateway address. Change when use another network
 byte mask[] = { 255, 255, 255, 0 }; // Mask. Change when use another network
 
 EthernetServer server(5000); // Port 5000 to use on this socket, must be the same on client
 
+String tmp = "24 C";
+String hmd = "35%";
+String auth = "";
+
 boolean alreadySent = true;
 String recvMSG = "";
-String sendMSG = "Hello, client!";
+String sendMSG = "";
 
 void setup() {
   Ethernet.begin(mac, ip, gateway, mask);
   server.begin();
   Serial.begin(9600);
+  pinMode(RED, OUTPUT);
+  pinMode(YELLOW, OUTPUT);
+  pinMode(BLUE, OUTPUT);
   
   Serial.print("Server address: ");
   Serial.println(Ethernet.localIP());
